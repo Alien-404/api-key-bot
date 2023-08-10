30ad6c36822298e4f57e323ea14e13909e3b9199
Alien-404 <ricoe72@gmail.com>
2023-08-10 15:05:29 +0700

    first commit

--- src/pages/api/key.js
+++ src/pages/api/key.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res
+    .status(200)
+    .json({
+      key: '$2a$12$IXYOhSk8/6mT6Uo2av91Le71b5mUPCkta4Jhq8MMjH5X3AsKyb2jS',
+    });
 }
